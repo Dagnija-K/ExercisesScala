3a3d0e9ca874d88bc25f96d7d33cc2fc3a34f45d
Dagnija-K <dagnijakaleja@gmail.com>
2021-02-16 20:57:00 +0200

    completed

--- src/main/scala/CharCounterExercise.scala
+++ src/main/scala/CharCounterExercise.scala
@@ -3,7 +3,7 @@ import scala.io.StdIn.readLine
 object CharCounterExercise extends App {
 val name = readLine("Hello! What is your name?")
   val example = readLine(s"$name, please write a sentence!")
-//  solution Aliaxander from https://stackoverflow.com/questions/37105995/count-char-frequency-in-string-with-scala
+//  idea from Aliaxander at https://stackoverflow.com/questions/37105995/count-char-frequency-in-string-with-scala
   val map = scala.collection.mutable.HashMap.empty[Char, Int]
   for (symbol <- example.toLowerCase.replace(" ", "")) {
     if (map.contains(symbol))
@@ -11,6 +11,7 @@ val name = readLine("Hello! What is your name?")
     else
       map.+=((symbol, 1))
   }
+  println("You will now see the character frequency in your sentence.")
   println(map)
 
 }
